b67186156d475f7692ac9be42fc0ecbc38452cc1
gilmour59 <gilmouralmalbisdev@gmail.com>
2021-02-27 17:56:48 +0800

    added sidebar

---
+++ views/modules/header.php
@@ -0,0 +1,25 @@
+<header class="main-header">
+
+    <a href="" class="logo">
+
+        <!--logo mini-->
+        <span class="logo-mini">
+            <img src="/views/img/template/icono-blanco.png" class="img-responsive" style="padding:10px">
+        </span>
+
+        <!--logo normal-->
+        <span class="logo-lg">
+            <img src="/views/img/template/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+        </span>
+    </a>
+
+    <!--Navigation bar-->
+    <nav class="navbar navbar-static-top" role="navigation">
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle Navigation</span>
+            <!-- <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> -->
+        </a>
+    </nav>
+</header>
--- views/template.php
+++ views/template.php
@@ -10,38 +10,38 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="views/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/views/bower_components/bootstrap/dist/css/bootstrap.min.css">
   
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="views/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/views/bower_components/font-awesome/css/font-awesome.min.css">
   
   <!-- Ionicons -->
-  <link rel="stylesheet" href="views/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="/views/bower_components/Ionicons/css/ionicons.min.css">
   
   <!-- Theme style -->
-  <link rel="stylesheet" href="views/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="/views/dist/css/AdminLTE.min.css">
   
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="views/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="/views/dist/css/skins/_all-skins.min.css">
 
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
   <!-- jQuery 3 -->
-  <script src="views/bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="/views/bower_components/jquery/dist/jquery.min.js"></script>
 
   <!-- Bootstrap 3.3.7 -->
-  <script src="views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+  <script src="/views/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
   <!-- SlimScroll -->
-  <script src="views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+  <script src="/views/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 
   <!-- FastClick -->
-  <script src="views/bower_components/fastclick/lib/fastclick.js"></script>
+  <script src="/views/bower_components/fastclick/lib/fastclick.js"></script>
 
   <!-- AdminLTE App -->
-  <script src="views/dist/js/adminlte.min.js"></script>
+  <script src="/views/dist/js/adminlte.min.js"></script>
 </head>
 
 <body class="hold-transition skin-blue sidebar-collapse sidebar-mini">
@@ -49,9 +49,13 @@
 <!-- Site wrapper -->
 <div class="wrapper">
 
+  <?php
+    include "modules/header.php";
+  ?>
+
 </div>
 <!-- ./wrapper -->
 
-<script src="js/template.js"></script>
+<script src="/views/js/template.js"></script>
 </body>
 </html>
