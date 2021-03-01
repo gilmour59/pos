4609f712cf56bb7b42fe150d4fd75d5bf66e1371
Gilmour Almalbis <gilmouralmalbisdev@gmail.com>
2021-03-01 16:51:33 +0800

    Added Add user modal

--- views/modules/users.php
+++ views/modules/users.php
@@ -1,44 +1,118 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Manage Users
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Manage Users</li>
-      </ol>
-    </section>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Manage Users
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Manage Users</li>
+    </ol>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <!-- Main content -->
+  <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAddUser">
+          Add User
+        </button>
       </div>
-      <!-- /.box -->
+      <div class="box-body">
+        <table class="table table-bordered table-striped">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Picture</th>
+              <th>Role</th>
+              <th>State</th>
+              <th>Last Login</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
-    </section>
-    <!-- /.content -->
+<!-- Modal Add User -->
+<div id="modalAddUser" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header" style="background:#3c8dbc; color:white">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Add User</h4>
+      </div>
+      <div class="modal-body">
+        <div class="box-body">
+
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-user"></i></span>
+              <input type="text" name="addName" class="form-control input-lg" placeholder="Insert Name Here" required>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-key"></i></span>
+              <input type="text" name="addUsername" class="form-control input-lg" placeholder="Insert Username Here" required>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+              <input type="password" name="addPassword" class="form-control input-lg" placeholder="Insert Password Here" required>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="input-group">
+              <span class="input-group-addon"><i class="fa fa-user"></i></span>
+              <select name="addRole" class="form-control input-lg">
+                <option value="">Select Role</option>
+                <option value="administrator">Administrator</option>
+                <option value="special">Special</option>
+                <option value="seller">Seller</option>
+              </select>
+            </div>
+          </div>
+
+          <div class="form-group">
+            <div class="panel panel-default">
+              <div class="panel-heading">Add Picture</div>
+              <div class="panel-body">
+                <input type="file" name="addPicture" id="addPicture">
+                <p class="help-block">Maximum of 200 MB</p>
+                <img src="/views/img/users/default/anonymous.png" class="img-thumbnail" width="100px">
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
   </div>
-  <!-- /.content-wrapper -->
+</div>
