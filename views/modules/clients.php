22d4c8c7f36ca41d9dd02bb18ce7ebd324e8b433
gilmour59 <gilmouralmalbisdev@gmail.com>
2021-03-07 00:38:37 +0800

    initial client

--- views/modules/clients.php
+++ views/modules/clients.php
@@ -1,44 +1,81 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Manage Clients
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Manage Clients</li>
-      </ol>
-    </section>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Manage Categories
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Manage Categories</li>
+    </ol>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <!-- Main content -->
+  <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#modalAddCategory">
+          Add Category
+        </button>
+      </div>
+      <div class="box-body">
+        <table class="table table-bordered table-striped dt-responsive datatable-user">
+          <thead>
+            <tr>
+              <th style="width:10px;">#</th>
+              <th>Category</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
 
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
+<!-- Modal Add Category -->
+<div id="modalAddCategory" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <form role="form" method="post">
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Add Category</h4>
+        </div>
+        <div class="modal-body">
+          <div class="box-body">
+
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-th"></i></span>
+                <input type="text" id="addCategory" name="addCategory" class="form-control input-lg" placeholder="Insert Category Here" required>
+              </div>
+            </div>
+
           </div>
         </div>
-        <div class="box-body">
-          Start creating your amazing application!
+        <div class="modal-footer">        
+          <button type="button" class="btn btn-danger pull-left" data-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Save Changes</button>
         </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
-      </div>
-      <!-- /.box -->
+      </form>
+    </div>
 
-    </section>
-    <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
+</div>
