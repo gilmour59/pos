7e4f544d173cf405b91378e0385a0572e5b23791
gilmour59 <gilmouralmalbisdev@gmail.com>
2021-03-07 15:47:17 +0800

    initial sale

--- views/modules/sales.php
+++ views/modules/sales.php
@@ -1,44 +1,65 @@
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Manage Sales
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Manage Sales</li>
-      </ol>
-    </section>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>
+      Manage Sales
+    </h1>
+    <ol class="breadcrumb">
+      <li><a href="home"><i class="fa fa-dashboard"></i> Home</a></li>
+      <li class="active">Manage Sales</li>
+    </ol>
+  </section>
 
-    <!-- Main content -->
-    <section class="content">
+  <!-- Main content -->
+  <section class="content">
 
-      <!-- Default box -->
-      <div class="box">
-        <div class="box-header with-border">
-          <h3 class="box-title">Title</h3>
-
-          <div class="box-tools pull-right">
-            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                    title="Collapse">
-              <i class="fa fa-minus"></i></button>
-            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-              <i class="fa fa-times"></i></button>
-          </div>
-        </div>
-        <div class="box-body">
-          Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-          Footer
-        </div>
-        <!-- /.box-footer-->
+    <!-- Default box -->
+    <div class="box">
+      <div class="box-header with-border">
+        <a href="sales-create">
+          <button class="btn btn-primary">
+            Add Sale
+          </button>
+        </a>        
       </div>
-      <!-- /.box -->
-
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+      <div class="box-body">
+        <table class="table table-bordered table-striped dt-responsive datatable-user">
+          <thead>
+            <tr>
+              <th style="width:10px;">#</th>
+              <th>Billing Code</th>
+              <th>Client</th>
+              <th>Seller</th>
+              <th>Form of Payment</th>
+              <th>Net Price</th>
+              <th>Total Price</th>
+              <th>Date</th>              
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>
+                <button class="btn btn-info"><i class="fa fa-print"></i></button>
+                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <!-- /.box-body -->
+    </div>
+    <!-- /.box -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
