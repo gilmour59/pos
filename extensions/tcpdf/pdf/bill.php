1fdc034dd1a040cb536551e92e28025eb21d29d4
gilmour59 <gilmouralmalbisdev@gmail.com>
2021-03-18 16:17:57 +0800

    cont 3rd block pdf

--- extensions/tcpdf/pdf/bill.php
+++ extensions/tcpdf/pdf/bill.php
@@ -3,23 +3,43 @@
 require_once '../../../controllers/sales.controller.php';
 require_once '../../../models/sales.model.php';
 
+require_once '../../../controllers/users.controller.php';
+require_once '../../../models/users.model.php';
+
+require_once '../../../controllers/clients.controller.php';
+require_once '../../../models/clients.model.php';
+
+require_once '../../../controllers/products.controller.php';
+require_once '../../../models/products.model.php';
 
 class PrintBills{
 
-    public $sale_code;
+    public $sale_code;    
 
     public function printBill(){
         
-        $item = "code";
-        $value = $this->sale_code;
+        $item_sale = "code";
+        $value_sale = $this->sale_code;
 
-        $result_sale = SaleController::ctrShowSales($item, $value);
+        $result_sale = SaleController::ctrShowSales($item_sale, $value_sale);
 
         $date = substr($result_sale['sale_date'], 0, -8);
         $products = json_decode($result_sale['products'], true);
         $net_price = number_format($result_sale['net_price'], 2);
         $tax = number_format($result_sale['tax'], 2);
         $total_price = number_format($result_sale['total_price'], 2);
+
+        //Seller Info
+        $item_seller = "id";
+        $value_seller = $result_sale['seller_id'];
+
+        $result_seller = UserController::ctrShowUsers($item_seller, $value_seller);
+
+        //Client Info
+        $item_client = "id";
+        $value_client = $result_sale['client_id'];
+
+        $result_client = ClientController::ctrShowClients($item_client, $value_client);
 
 //TCPPDF Needs to have no spaces for it to work
 
@@ -34,6 +54,8 @@ $pdf->startPageGroup();
 //Portrait and A4 Size
 $pdf->AddPage('P', 'A4');
 
+//------------------------------------------------------------------------------------------------
+//FIRST BLOCK
 //EOF is like a tag
 $block_1 = <<<EOF
 
@@ -67,6 +89,41 @@ $block_1 = <<<EOF
 EOF;
 
 $pdf->writeHTML($block_1, false, false, false, false, '');
+
+//------------------------------------------------------------------------------------------------
+//SECOND BLOCK
+$block_2 = <<<EOF
+
+    <table>
+        <tr>
+            <td style="width:540px">
+                <img src="images/back.jpg">
+            </td>
+        </tr>
+    </table>
+
+    <table style="font-size:10px; padding:5px 10px;">
+        <tr>
+            <td style="border:1px solid #666; background-color:white; width:390px">
+                Client: $result_client[name]
+            </td>
+            <td style="border:1px solid #666; background-color:white; width:150px text-align:right">
+                Date: $date
+            </td>
+        </tr>
+        <tr>
+            <td style="border:1px solid #666; background-color:white; width:540px">
+                Seller: $result_seller[name]
+            </td>            
+        </tr>
+    </table>
+
+EOF;
+
+$pdf->writeHTML($block_2, false, false, false, false, '');
+
+//------------------------------------------------------------------------------------------------
+//THIRD BLOCK
 
 //for the pdf to be seen
 $pdf->Output('bill.pdf');
